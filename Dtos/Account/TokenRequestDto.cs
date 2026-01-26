c9d2273b18124a781ab6febaac07f7976cc5d182
Gradi Puata <gradipuata@gmail.com>
2026-01-26 20:54:00 +0200

    Added validation on token dto

--- Dtos/Account/TokenRequestDto.cs
+++ Dtos/Account/TokenRequestDto.cs
@@ -1,5 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 public class TokenRequestDto
 {
+  [Required]
   public string Token { get; set; } = string.Empty;
+  [Required]
   public string RefreshToken { get; set; } = string.Empty;
 }
