6773bee0154b81696087f806264b9b8c7e1f752d
Gradi Puata <gradipuata@gmail.com>
2026-02-02 22:00:55 +0200

    managing the order controller

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,7 +1,39 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-[Route("api/order")]
+[Route("api/[controller]")]
 [ApiController]
 public class OrderController : ControllerBase
 {
+  private readonly IOrderRepo _orderRepo;
+  private readonly UserManager<AppUser> _userManager;
+  public OrderController(UserManager<AppUser> userManager, IOrderRepo orderRepo)
+  {
+    _orderRepo = orderRepo;
+    _userManager = userManager;
+  }
+  [HttpGet]
+  [Authorize]
+  public async Task<IActionResult> GetAll()
+  {
 
+  }
+  [HttpGet("{id:int}")]
+  [Authorize]
+  public async Task<IActionResult> GetOne()
+  {
+
+  }
+  [HttpPost]
+  [Authorize]
+  public async Task<IActionResult> CreateOne()
+  {
+
+  }
+  [HttpPut("{id:int}")]
+  [Authorize]
+  public async Task<IActionResult> ShipOder()
+  {
+
+  }
 }
